cb272a709a9a00eea2f144eca565789f8438506e
Sebastian Zúñiga <sebastian.zunigas@bvlchile.com>
2025-02-25 15:15:34 -0300

    Se quito el config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# Token del bot de Discord (reemplaza esto con tu token real)
-DISCORD_TOKEN = "tu_token_de_discord"
-
-# URI de conexión a MongoDB (reemplaza si no usas localhost)
-MONGO_URI = "mongodb://localhost:27017/"
-
-# Nombre de la base de datos
-DATABASE_NAME = "futgol_bot"
