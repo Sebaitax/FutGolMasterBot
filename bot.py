5e7c0432f672a89a72695204158d00441eb3eb84
Dilan <dilan.fernandois.21@gmail.com>
2025-02-28 01:22:38 -0300

    skarner

--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
-#funciones locales
-from database import funciones
+from database import funciones as f
+
 
 intents = discord.Intents.default()
 intents.message_content = True  # Necesario para acceder al contenido de los mensajes
 intents.members = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="-", intents=intents)
 
 @bot.event
 async def bot_conect():
@@ -22,6 +22,13 @@ async def ayuda(ctx):
 @bot.command()
 async def start(ctx):
     await ctx.send("¡Bienvenido al FutGolMasterBot! Usa `!collect` para ver tus cartas.")
+    usuario_ex = False
+    if usuario_ex:
+        f.obtener_usuario()
+    else:
+        f.crear_usuario()
+
+
 
 
 @bot.command()
@@ -32,12 +39,6 @@ async def ver_inventario(ctx):
 async def crear_plantilla(ctx, nombre, *jugadores):
     await ctx.send(f"Plantilla {nombre} creada con los jugadores: {', '.join(jugadores)}")
 
-@bot.command()
-async def simular_partido(ctx, plantilla_1, plantilla_2):
-    eventos = simular_partido(plantilla_1, plantilla_2)
-    for evento in eventos:
-        await ctx.send(evento)
-    
 
 load_dotenv()
 
---
+++ models/userModel.py
@@ -0,0 +1,4 @@
+class User:
+    def __init__(self,nombre,precio):
+        self.nombre = nombre
+        self.precio = precio
